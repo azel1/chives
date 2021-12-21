442057765a4eaf9df67e293311c92169aa8fedff
azel1 <39050180+azel1@users.noreply.github.com>
2021-12-21 22:38:41 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -19,13 +19,13 @@ mkdir ~/gdrive5
 mkdir ~/gdrive6
 mkdir ~/gdrive7
 apt install screen
-screen -S gdrive -dm bash -c "echo 'Mounting gdrive';rclone mount gdrive:chiamain ~/gdrive"
-screen -S gdrive2 -dm bash -c "echo 'Mounting gdrive2';rclone mount gdrive:chiamain ~/gdrive2"
-screen -S gdrive3 -dm bash -c "echo 'Mounting gdrive3';rclone mount gdrive:chiamain ~/gdrive3"
-screen -S gdrive4 -dm bash -c "echo 'Mounting gdrive4';rclone mount gdrive:chiamain ~/gdrive4"
-screen -S gdrive5 -dm bash -c "echo 'Mounting gdrive5';rclone mount gdrive:chiamain ~/gdrive5"
-screen -S gdrive6 -dm bash -c "echo 'Mounting gdrive6';rclone mount gdrive:chiamain ~/gdrive6"
-screen -S gdrive7 -dm bash -c "echo 'Mounting gdrive7';rclone mount gdrive:chiamain ~/gdrive7"
+screen -S gdrive -dm bash -c "echo 'Mounting gdrive';rclone mount gdrive:chiamain ~/gdrive --allow-non-empty"
+screen -S gdrive2 -dm bash -c "echo 'Mounting gdrive2';rclone mount gdrive:chiamain ~/gdrive2 --allow-non-empty"
+screen -S gdrive3 -dm bash -c "echo 'Mounting gdrive3';rclone mount gdrive:chiamain ~/gdrive3 --allow-non-empty"
+screen -S gdrive4 -dm bash -c "echo 'Mounting gdrive4';rclone mount gdrive:chiamain ~/gdrive4 --allow-non-empty"
+screen -S gdrive5 -dm bash -c "echo 'Mounting gdrive5';rclone mount gdrive:chiamain ~/gdrive5 --allow-non-empty"
+screen -S gdrive6 -dm bash -c "echo 'Mounting gdrive6';rclone mount gdrive:chiamain ~/gdrive6 --allow-non-empty"
+screen -S gdrive7 -dm bash -c "echo 'Mounting gdrive7';rclone mount gdrive:chiamain ~/gdrive7 --allow-non-empty"
 echo "Finished RClone Section"
 
 # MADMAX
